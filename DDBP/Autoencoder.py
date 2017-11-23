7028aeefb13c45d786d66a15e5db8c95ae9683d4
holgus103 <kubasuchan@hotmail.com>
2017-11-23 12:35:11 +0100

    Fixed compilation errors

--- DDBP/Autoencoder.py
+++ DDBP/Autoencoder.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 class Autoencoder:
 
     def createHiddenLayer(self, prevCount, currCount, input):
-            w = tf.Variable(tf.random_normal(prevCount, currCount]));
-            b = tf.Variable(tf.random_normal([currCount]);
+            w = tf.Variable(tf.random_normal([prevCount, currCount]));
+            b = tf.Variable(tf.random_normal([currCount]));
             self.weights.append(w);
             self.biases.append(b);
             self.layers.append(tf.nn.sigmoid(tf.add(tf.matmul(input, w), b)));
@@ -14,18 +14,14 @@ class Autoencoder:
         self.weights = [];   
         self.biases = [];
         self.layers = [];
-        l = len(inputs, layerCounts);
+        l = len(layerCounts);
         
-        self.createHiddenLayer(inputCount[0], inputCount[1], input);
+        self.createHiddenLayer(inputCount, layerCounts[0], input);
         # add encoding layers
         for i in range(1, l - 1):
             self.createHiddenLayer(layerCounts[i-1], layerCounts[i], self.layers[i - 1]);
-            #self.weights.append(tf.Variable(tf.random_normal([layerCounts[i], layerCounts[i + 1]])));
-            #self.biases.append(tf.Variable(tf.random_normal([layerCount[i]])));
-            #self.layers.append(tf.nn.sigmoid(tf.add(tf.matmul(, weights[))))
         
         # add decoding layers
-        for i in range(1, l - 2)):
-            self.createHiddenLayer(layerCounts[l - i - 1], layerCounts[; - i - 2], self.layers[i - 1]);
-            #self.weights.append(tf.Variable(tf.random_normal([layerCounts[l - i - 1], layerCounts[l - i - 2])));
-            #self.biases.append(tf.Variable(tf.random_normal([layerCount[i]])));
+        for i in range(1, l - 2):
+            self.createHiddenLayer(layerCounts[l - i - 1], layerCounts[l - i - 2], self.layers[i - 1]);
+    
--- DDBP/DDBP.py
+++ DDBP/DDBP.py
@@ -1,1 +1,4 @@
+import Autoencoder;
+import tensorflow as tf;
 
+a = Autoencoder.Autoencoder(10, [8,2], tf.Variable(tf.random_normal([1,  10])));
