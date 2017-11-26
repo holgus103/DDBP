461f4aae252d64d8365b33654d6773f14ee2fc24
holgus103 <kubasuchan@hotmail.com>
2017-11-26 18:03:08 +0100

    Fix - building Autoencoder subnetworks

--- DDBP/Autoencoder.py
+++ DDBP/Autoencoder.py
@@ -6,16 +6,16 @@ class Autoencoder:
         return tf.reduce_mean(tf.pow(actual - pred, 2));
 
     def crossEntropyLoss(pred, actual):
-        crossEntropy = tf.add(tf.mul(tf.log(pred), actual), tf.mul(tf.log(1 - pred), 1 - actual));
+        crossEntropy = tf.add(tf.multiply(tf.log(pred), actual), tf.multiply(tf.log(1 - pred), 1 - actual));
         return -tf.reduce_mean(tf.reduce_sum(crossEntropy, 1));
 
     def createLayer(self, index, input, isFixed = False, isDecoder = False):
         if isFixed:
             if isDecoder:
                 return tf.nn.sigmoid(tf.add(tf.matmul(input, self.fixedWeights[index], transpose_b = isDecoder), self.outBiasesFixed[index]));
-            return tf.nn.sigmoid(tf.add(tf.matmul(input, self.fixedWeights[index], transpose_b = isDecoder), self.fixedBiases[index]));
+            return tf.nn.sigmoid(tf.add(tf.matmul(input, self.fixedWeights[index], transpose_b = isDecoder), self.biases[index]));
         if isDecoder:
-            return tf.nn.sigmoid(tf.add(tf.matmul(input, self.fixedWeights[index], transpose_b = isDecoder), self.outBiasesFixed[index]));
+            return tf.nn.sigmoid(tf.add(tf.matmul(input, self.weights[index], transpose_b = isDecoder), self.outBiasesFixed[index]));
         return tf.nn.sigmoid(tf.add(tf.matmul(input, self.weights[index], transpose_b = isDecoder), self.biases[index]));
 
     def createWeights(self, prevCount, currCount):
@@ -58,22 +58,27 @@ class Autoencoder:
         # add output layer
         #self.createWeights(layerCounts[0], inputCount);
 
-    def train(data, desiredOutput, learningRate, it, batchsize,):
+    #def train(data, desiredOutput, learningRate, it, batchsize,):
+
+
+
+
+
+    
+    def pretrain(self, learningRate):
         init = tf.global_variables_initializer();
 
-        lossFunction = self.loss(self.layers[len(self.layers) - 1], self.input);
-        optimizer = tf.train.RMSPropOptimizer(learningRate).minimize(lossFunction);
-
-        with tf.Session() as session:
-             sess.run(init);
-            
-
-             for i in range(1, it):
-                 sess.run([optimizer, lossFunction], feed_dict={self.input : data});
-    
-    def pretrain(self):
         for i in range(0, len(self.layerCounts)):
-            net = self.buildPretrainNet(i)
+            net = self.buildPretrainNet(i);
+            #lossFunction = self.loss(net[len(net) - 1], self.input);
+            #optimizer = tf.train.RMSPropOptimizer(learningRate).minimize(lossFunction);
+            #with tf.Session() as session:
+            #     sess.run(init);         
+            #     for i in range(1, it):
+            #         sess.run([optimizer, lossFunction], feed_dict={self.input : data});
+
+
+            
 
     def buildPretrainNet(self, n):
         
@@ -90,7 +95,7 @@ class Autoencoder:
         layers.append(inp);
         
         for i in range(0, n):
-            inp = self.createLayer(n - i, inp, isFixed = True, isDecoder = True);
+            inp = self.createLayer(n - 1 - i, inp, isFixed = True, isDecoder = True);
             layers.append(inp);
         return layers;
             
--- DDBP/DDBP.py
+++ DDBP/DDBP.py
@@ -1,5 +1,6 @@
 import Autoencoder;
 import tensorflow as tf;
 
+learningRate = 0.01;
 a = Autoencoder.Autoencoder(10, [8,2], Autoencoder.Autoencoder.crossEntropyLoss);
-a.pretrain();
+a.pretrain(learningRate);
