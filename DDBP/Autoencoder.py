Parser, Batches and Gradient descent - removed multiple global variable initializations - optimizer changed to GradientDescent - minor bug fixes

DDBP/Autoencoder.py
@@ -9,7 +9,7 @@ class Autoencoder:
         return -tf.reduce_mean(tf.reduce_sum(crossEntropy, 1));
 
     @property
-    def session(self)
+    def session(self):
         return self.__session;
 
     def createLayer(self, index, input, isFixed = False, isDecoder = False):
@@ -74,25 +74,25 @@ class Autoencoder:
 
      
     def pretrain(self, learningRate, it, data):
-        self.session = tf.Session();
+        self.__session = tf.Session();
+        init = tf.global_variables_initializer();
+        self.session.run(init);
+        for i in range(0, len(self.layerCounts)):
+            #with tf.Graph().as_default() as g:
+            input = tf.placeholder("float", [len(data), self.inputCount]);
+            net = self.buildPretrainNet(i, input);
+            lossFunction = self.loss(net[len(net) - 1], input);
+            optimizer = tf.train.GradientDescentOptimizer(learningRate).minimize(lossFunction);
 
-            for i in range(0, len(self.layerCounts)):
-                #with tf.Graph().as_default() as g:
-                    input = tf.placeholder("float", [None, self.inputCount]);
-                    net = self.buildPretrainNet(i, input);
-                    lossFunction = self.loss(net[len(net) - 1], input);
-                    optimizer = tf.train.RMSPropOptimizer(learningRate).minimize(lossFunction);
-                    init = tf.global_variables_initializer();
-                    session.run(init);
-                    session.run(tf.initialize_variables(self.getVariablesToInit(i)));         
-                    for i in range(1, it):
-                            _, loss = session.run([optimizer, lossFunction], feed_dict={input : data});
+            self.session.run(tf.initialize_variables(self.getVariablesToInit(i)));         
+            for i in range(1, it):
+                    _, loss = self.session.run([optimizer, lossFunction], feed_dict={input : data});
             
     def buildCompleteNet(self, input):
         net = [];
         inp = input;
         for i in range(0, len(self.weights)):
-            inp = self.createLayer(i, input);
+            inp = self.createLayer(i, inp);
             net.append(inp);
             
         return net;

DDBP/Classifier.py
@@ -3,22 +3,22 @@ import tensorflow as tf;
 class Classifier:
     """description of class"""
     def __init__(self, autoencoder, outputs):
-        self.autoencoder = autonencoder;
+        self.autoencoder = autoencoder;
         self.inputPlaceholder = tf.placeholder("float", [None, self.autoencoder.inputCount]);
         self.encoder = autoencoder.buildCompleteNet(self.inputPlaceholder);
-        input = encoder[len(encoder) - 1];
+        input = self.encoder[len(self.encoder) - 1];
         self.weights = tf.Variable(tf.random_normal([input.shape[1].value, outputs]));
-        self.biases = tf.Variable(tf.random_normal([input.shape[1].value));
+        self.biases = tf.Variable(tf.random_normal([outputs]));
         self.layer = tf.nn.softmax(tf.matmul(input, self.weights) + self.biases);
         self.outputPlaceholder = tf.placeholder("float", [None, outputs]);
         
 
     def train(self, data, desiredOutput, learningRate, it):
-        loss = tf.reduce_mean(tf.pow(self.layer - desiredOutput, 2));
-        optimizer = tf.train.RMSPropOptimizer(learningRate).minimize(lossFunction);
-        self.autoencoder.session.run(tf.initialize_variables([self.weights, self.layer, self.biases, loss, optimizer]));
+        loss = tf.reduce_mean(tf.pow(self.layer - self.outputPlaceholder, 2));
+        optimizer = tf.train.GradientDescentOptimizer(learningRate).minimize(loss);
+        self.autoencoder.session.run(tf.initialize_variables([self.weights, self.biases]));
         for i in range(0, it):
-            self.autoencoder.session.run([optimizer, lossFunction], feed_dict={self.inputPlaceholder: data, self.outputPlaceholder: desiredOutput});
+            self.autoencoder.session.run([optimizer, loss], feed_dict={self.inputPlaceholder: data, self.outputPlaceholder: desiredOutput});
 
         
         