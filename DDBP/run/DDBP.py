8dc064ea6303057d53ceac12263376589c1d4f80
Ubuntu <ubuntu@ip-172-31-43-206.us-west-2.compute.internal>
2018-03-31 14:27:20 +0000

    no_trump_rotations_104enc_eta=0.002, 400k

--- DDBP/run/DDBP.py
+++ DDBP/run/DDBP.py
@@ -8,14 +8,14 @@ import pprint;
 
 
 # configure here
-TEST_TRUMP = True
-TRAIN_TRUMP = True
-TEST_NO_TRUMP = False
-TRAIN_NO_TRUMP = False
+TEST_TRUMP = False
+TRAIN_TRUMP = False
+TEST_NO_TRUMP = True
+TRAIN_NO_TRUMP = True
 BATCHES = 4
-PARTITION = 0.5
-SET_SIZE = 200000
-EXPERIMENT = "trump_rotations_104enc_quick"
+PARTITION = 0.66
+SET_SIZE = 600000
+EXPERIMENT = "no_trump_rotations_104enc_eta=0.002, 400k"
 
 
 
@@ -66,7 +66,7 @@ a.pretrain(0.001, 0, 200, data_batches, 0, 0.01, path + "{0}" , optimizer, 0.2, 
 # create classifier
 c = models.Classifier(a, [52, 13, 14]);
 # train whole network
-c.train(data_batches, outputs_batches, 0.001, 15000, 0, path +"/finetuning", data, outputs, test_data, test_outputs, dp.suit_count_for_params(TRAIN_NO_TRUMP, TRAIN_TRUMP), dp.suit_count_for_params(TEST_NO_TRUMP, TEST_TRUMP), models.Model.mse_loss, 25, experiment_name);
+c.train(data_batches, outputs_batches, 0.002, 15000, 0, path +"/finetuning", data, outputs, test_data, test_outputs, dp.suit_count_for_params(TRAIN_NO_TRUMP, TRAIN_TRUMP), dp.suit_count_for_params(TEST_NO_TRUMP, TEST_TRUMP), models.Model.mse_loss, 25, experiment_name);
 
 # evaluate results
 print(c.test(data, outputs));
