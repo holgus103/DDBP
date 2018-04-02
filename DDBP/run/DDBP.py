a20ba74cc275ee44a2005e3581a2ffc06ef5f50e
holgus103 <kubasuchan@hotmail.com>
2018-04-02 08:56:58 +0200

    trump_altered__fixed_104enc_eta=0.004

--- DDBP/run/DDBP.py
+++ DDBP/run/DDBP.py
@@ -8,15 +8,15 @@ import pprint;
 
 
 # configure here
-TEST_TRUMP = False
-TRAIN_TRUMP = False
-TEST_NO_TRUMP = True
-TRAIN_NO_TRUMP = True
+TEST_TRUMP = True
+TRAIN_TRUMP = True
+TEST_NO_TRUMP = False
+TRAIN_NO_TRUMP = False
 BATCHES = 4
-PARTITION = 0.66
-SET_SIZE = 600
-SECOND_LAYER = 39
-EXPERIMENT = "no_trump_altered_104enc_eta=0.002"
+PARTITION = 0.5
+SET_SIZE = 200000
+SECOND_LAYER = 26
+EXPERIMENT = "trump_altered__fixed_104enc_eta=0.004"
 
 
 
@@ -32,7 +32,7 @@ path = "./summaries/{0}/".format(experiment_name);
 dp.initialize_random(experiment_name);
 
 # import data
-(data, outputs, test_data, test_outputs) = dp.read_file("./../data/library", SET_SIZE, True, TRAIN_NO_TRUMP, TRAIN_TRUMP, TEST_NO_TRUMP, TEST_TRUMP, PARTITION);
+(data, outputs, test_data, test_outputs) = dp.read_file("./data/library", SET_SIZE, True, TRAIN_NO_TRUMP, TRAIN_TRUMP, TEST_NO_TRUMP, TEST_TRUMP, PARTITION);
 
 d_train = dp.get_distribution(data, outputs);
 d_test = dp.get_distribution(test_data, test_outputs);
@@ -63,12 +63,12 @@ a = models.Autoencoder(models.Model.cross_entropy_loss, SECOND_LAYER);
 
 
 # pretrain each layer
-a.pretrain(0.001, 0, 200, data_batches, 0, -0.01, path + "{0}" , optimizer, 0.2, 15);
+a.pretrain(0.001, 0, 200, data_batches, 0, 0.01, path + "{0}" , optimizer, 0.2, 15);
 
 # create classifier
 c = models.Classifier(a, [52, 13, 14]);
 # train whole network
-c.train(data_batches, outputs_batches, 0.002, 15000, 0, path +"/finetuning", data, outputs, test_data, test_outputs, dp.suit_count_for_params(TRAIN_NO_TRUMP, TRAIN_TRUMP), dp.suit_count_for_params(TEST_NO_TRUMP, TEST_TRUMP), models.Model.mse_loss, 25, experiment_name);
+c.train(data_batches, outputs_batches, 0.004, 15000, 0, path +"/finetuning", data, outputs, test_data, test_outputs, dp.suit_count_for_params(TRAIN_NO_TRUMP, TRAIN_TRUMP), dp.suit_count_for_params(TEST_NO_TRUMP, TEST_TRUMP), models.Model.mse_loss, 25, experiment_name);
 
 # evaluate results
 print(c.test(data, outputs));
